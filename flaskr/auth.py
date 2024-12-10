f360c5724f9d7e86250bdd186e286bd2aa77876e
shakti624 <ep22b007@smail.iitm.ac.in>
2024-12-10 15:10:10 +0530

    adding testing for chatbot

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -41,4 +41,42 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None 
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'wrong username'
+        elif not check_password_hash(user['password'], password):
+            error = 'wrong password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db.execute(
+            'SELECT * FROM user where ID = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
